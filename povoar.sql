ec53fdddcc4b27a2135bd1228254ad5c7675e382
marco-vb <marcofilipe03@hotmail.com>
2022-11-17 20:16:14 +0000

    increased population

---
+++ povoar.sql
@@ -0,0 +1,59 @@
+PRAGMA foreign_keys = on;
+
+INSERT INTO league VALUES (2023, 'BWIN');
+
+INSERT INTO round_ VALUES (1, 2023);
+INSERT INTO round_ VALUES (2, 2023);
+INSERT INTO round_ VALUES (3, 2023);
+INSERT INTO round_ VALUES (4, 2023);
+INSERT INTO round_ VALUES (5, 2023);
+INSERT INTO round_ VALUES (6, 2023);
+INSERT INTO round_ VALUES (7, 2023);
+INSERT INTO round_ VALUES (8, 2023);
+INSERT INTO round_ VALUES (9, 2023);
+INSERT INTO round_ VALUES (10, 2023);
+INSERT INTO round_ VALUES (11, 2023);
+INSERT INTO round_ VALUES (12, 2023);
+INSERT INTO round_ VALUES (13, 2023);
+INSERT INTO round_ VALUES (14, 2023);
+INSERT INTO round_ VALUES (15, 2023);
+INSERT INTO round_ VALUES (16, 2023);
+INSERT INTO round_ VALUES (17, 2023);
+INSERT INTO round_ VALUES (18, 2023);
+INSERT INTO round_ VALUES (19, 2023);
+INSERT INTO round_ VALUES (20, 2023);
+INSERT INTO round_ VALUES (21, 2023);
+INSERT INTO round_ VALUES (22, 2023);
+INSERT INTO round_ VALUES (23, 2023);
+INSERT INTO round_ VALUES (24, 2023);
+INSERT INTO round_ VALUES (25, 2023);
+INSERT INTO round_ VALUES (26, 2023);
+INSERT INTO round_ VALUES (27, 2023);
+INSERT INTO round_ VALUES (28, 2023);
+INSERT INTO round_ VALUES (29, 2023);
+INSERT INTO round_ VALUES (30, 2023);
+INSERT INTO round_ VALUES (31, 2023);
+INSERT INTO round_ VALUES (32, 2023);
+INSERT INTO round_ VALUES (33, 2023);
+INSERT INTO round_ VALUES (34, 2023);
+
+//games
+
+INSERT INTO team VALUES ('Porto', 'Estádio do Dragão');
+INSERT INTO team VALUES ('Sporting', 'Estádio José Alvalade');
+INSERT INTO team VALUES ('Benfica', 'Estádio da Luz');
+INSERT INTO team VALUES ('Braga', 'Estádio Municipal de Braga');
+INSERT INTO team VALUES ('Boavista', 'Estádio do Bessa');
+INSERT INTO team VALUES ('Vitória', 'Estádio do Bonfim');
+INSERT INTO team VALUES ('Paços de Ferreira', 'Estádio Municipal de Paços de Ferreira');
+INSERT INTO team VALUES ('Arouca', 'Estádio Municipal de Arouca');
+INSERT INTO team VALUES ('Vizela', 'Estádio Municipal de Vizela');
+INSERT INTO team VALUES ('Famalicão', 'Estádio Municipal de Famalicão');
+INSERT INTO team VALUES ('Santa Clara', 'Estádio de São Miguel');
+INSERT INTO team VALUES ('Chaves', 'Estádio Municipal de Chaves');
+INSERT INTO team VALUES ('Estoril Praia', 'Estádio António Coimbra da Mota');
+INSERT INTO team VALUES ('Gil Vicente', 'Estádio Cidade de Barcelos');
+INSERT INTO team VALUES ('Marítimo', 'Estádio dos Barreiros');
+INSERT INTO team VALUES ('Casa Pia', 'Estádio Municipal de Alvalade');
+INSERT INTO team VALUES ('Rio Ave', 'Estádio do Rio Ave');
+INSERT INTO team VALUES ('Portimonense', 'Estádio Municipal de Portimão');
