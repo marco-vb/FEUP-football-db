18a750233f2bf5b1af573a770c6086d4ac05ca37
marco-vb <marcofilipe03@hotmail.com>
2022-11-18 17:32:34 +0000

    updated creation

--- criar.sql
+++ criar.sql
@@ -29,6 +29,8 @@ create table round_ (
     league_yr integer not null,
     constraint round__pk primary key (nr),
     constraint round__fk foreign key (league_yr) references league (yr)
+    on delete cascade
+    on update restrict
 );  
 
 create table team (  
@@ -36,6 +38,8 @@ create table team (
     stadium_name varchar(50) not null,
     constraint team_pk primary key (name_),
     constraint team_fk foreign key (stadium_name) references stadium (name_)
+    on delete cascade
+    on update restrict
 );  
 
 create table stadium (  
@@ -45,6 +49,8 @@ create table stadium (
     team_name varchar(50) not null,  
     constraint stadium_pk primary key (name_),  
     constraint stadium_fk foreign key (team_name) references team (name_)
+    on delete cascade
+    on update restrict
 );  
 
 create table person (  
@@ -59,22 +65,32 @@ create table player (
     nr integer,  
     team_name varchar(50) not null,  
     constraint player_pk primary key (id),
-    constraint player_fk foreign key (id) references person (id),  
+    constraint player_fk foreign key (id) references person (id)
+    on delete cascade
+    on update restrict,
     constraint player_fk foreign key (team_name) references team (name_)
+    on delete cascade
+    on update restrict
 );  
 
 create table coach (  
     id integer not null,  
     team_name varchar(50) not null,  
     constraint coach_pk primary key (id),
-    constraint coach_fk foreign key (id) references person (id),
+    constraint coach_fk foreign key (id) references person (id)
+    on delete cascade
+    on update restrict,
     constraint coach_fk foreign key (team_name) references team (name_)
+    on delete cascade
+    on update restrict
 );  
 
 create table referee (  
     id integer not null,  
     constraint referee_pk primary key (id),
     constraint referee_fk foreign key (id) references person (id)
+    on delete cascade
+    on update restrict
 );  
 
 create table game (  
@@ -86,11 +102,21 @@ create table game (
     away_team_name varchar(50) not null,  
     round_id integer not null,  
     constraint game_pk primary key (id),  
-    constraint game_fk1 foreign key (stadium_name) references stadium (name_),  
-    constraint game_fk2 foreign key (referee_id) references referee (id),  
-    constraint game_fk3 foreign key (home_team_name) references team (name_),  
-    constraint game_fk4 foreign key (away_team_name) references team (name_),  
+    constraint game_fk1 foreign key (stadium_name) references stadium (name_)
+    on delete cascade
+    on update restrict,  
+    constraint game_fk2 foreign key (referee_id) references referee (id)
+    on delete cascade
+    on update restrict,  
+    constraint game_fk3 foreign key (home_team_name) references team (name_)
+    on delete cascade
+    on update restrict,  
+    constraint game_fk4 foreign key (away_team_name) references team (name_)
+    on delete cascade
+    on update restrict,  
     constraint game_fk5 foreign key (round_id) references round_ (nr)
+    on delete cascade
+    on update restrict
 ); 
 
 create table event_ (  
@@ -98,8 +124,12 @@ create table event_ (
     player_id integer not null,  
     game_id integer not null,  
     constraint event__pk primary key (id),  
-    constraint event__fk1 foreign key (player_id) references player (id),  
+    constraint event__fk1 foreign key (player_id) references player (id)
+    on delete cascade
+    on update restrict,  
     constraint event__fk2 foreign key (game_id) references game (id)
+    on delete cascade
+    on update restrict
 );  
 
 create table goal (  
@@ -107,16 +137,22 @@ create table goal (
     own_goal boolean default false not null,  
     constraint goal_pk primary key (id),
     constraint goal_fk foreign key (id) references event_ (id)
+    on delete cascade
+    on update restrict
 );  
 
 create table red_card (  
     id integer not null,  
     constraint red_card_pk primary key (id),
-    constraint red_card_fk foreign key (id) references event_ (id)  
+    constraint red_card_fk foreign key (id) references event_ (id)
+    on delete cascade
+    on update restrict
 );  
 
 create table yellow_card (  
     id integer not null,  
     constraint yellow_card_pk primary key (id),
     constraint yellow_card_fk foreign key (id) references event_ (id)
+    on delete cascade
+    on update restrict
 );
