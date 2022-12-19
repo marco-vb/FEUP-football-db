c80b9fa2dbd8c45135b666fe366150be5d3e13a7
marco-vb <marcofilipe03@hotmail.com>
2022-12-19 01:03:06 +0000

    gato 1

--- sql_files/gatilho1_adiciona.sql
+++ sql_files/gatilho1_adiciona.sql
@@ -4,13 +4,12 @@ when  (
         select count(*)
         from (
                 select e.id, e.player_id, e.game_id, t.player_id, t.game_id
-                from event_ e, yellow_card c,
-                (
+                from event_ e, yellow_card c, (
                     select player_id, game_id from event_ e, yellow_card c
                     where e.id = new.id and e.id = c.id
                 ) as t
                 where e.id = c.id and e.player_id = t.player_id and e.game_id = t.game_id
-            )
+        )
     ) >= 2
 begin
     insert into red_card (id) values (new.id);
