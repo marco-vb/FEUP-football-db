73eca178ca62ffa24847e053e959a1cdfac620da
Duarte Gonçalves <up202108772@edu.fe.up.pt>
2022-12-20 23:10:45 +0000

    Query 8 finished

--- sql_files/int8.sql
+++ sql_files/int8.sql
@@ -2,14 +2,11 @@
 .headers on
 .nullvalue NULL
 
---Retrieve the names and numbers of all players who have received more than 3 yellow cards until round 3
+--Retrieve the names and numbers of all players who have received 2 or more yellow cards until round 4
 
-SELECT p.name_, p1.nr
-FROM person p, player p1
-JOIN player pl ON p1.id = pl.id
-JOIN event_ e ON e.player_id = pl.id
-JOIN yellow_card yc ON yc.id = e.id
-JOIN game ga ON e.id = ga.id
-JOIN round_ r ON r.nr = ga.round_id
-WHERE r.nr = 1
-GROUP BY p.name_, p.age
+select p.team_name as TEAM, p.nr as NUMBER, person.name_ as NAME, count(*) as YELLOW_CARDS
+from player p, person, yellow_card y, event_ e, game g
+where p.id = person.id and p.id = e.player_id and y.id = e.id and e.game_id = g.id and g.round_id <= 4
+group by person.name_
+having count(*) >= 2
+order by p.team_name, p.nr
