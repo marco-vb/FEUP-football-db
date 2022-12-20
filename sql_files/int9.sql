11771dae58801957e85d7880905dcfd71f1256cc
marco-vb <marcofilipe03@hotmail.com>
2022-12-20 23:28:09 +0000

    Query 9 feita (os 5 melhores marcadores)

--- sql_files/int9.sql
+++ sql_files/int9.sql
@@ -1,3 +1,19 @@
-.mode columns
-.headers on
+.mode column
+.headers off
 .nullvalue NULL
+
+select "Os 5 melhores marcadores da liga";
+select "";
+
+.headers on
+
+select t.team_name as Team, person.name_ as Player, t.goals as Goals
+from person, (
+                select player_id, team_name, count(*) as goals
+                from event_, goal, player
+                where event_.id = goal.id and event_.player_id = player.id
+                group by 1
+            ) as t
+where person.id = t.player_id
+order by Goals desc
+limit 5;
