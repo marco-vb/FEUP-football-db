e52d29916082acdc6f5bf30b1ebb2d9cbaa7d890
marco-vb <marcofilipe03@hotmail.com>
2022-12-20 21:38:05 +0000

    More queries!

--- sql_files/int4.sql
+++ sql_files/int4.sql
@@ -1,42 +1,47 @@
-.mode columns
-.headers on
+.mode column
+.headers off
 .nullvalue NULL
 
--- list the classification table of all teams for round 6
+select "Classification table for round 6";
+select "";
 
-SELECT team.name_ as Name, ((SELECT COUNT(*) FROM
-    (SELECT game.date_ as Date, stadium.name_ as Stadium, home_team.name_ as Home_Team, away_team.name_ as Away_Team,
-       (SELECT COUNT(*) FROM event_
-        INNER JOIN player ON player.id = event_.player_id
-        INNER JOIN team ON team.name_ = player.team_name
-        INNER JOIN goal ON goal.id = event_.id
-        WHERE team.name_ = home_team.name_ AND event_.game_id = game.id AND event_.id = goal.id) AS Home_Team_Score,
-       (SELECT COUNT(*) FROM event_
-        INNER JOIN player ON player.id = event_.player_id
-        INNER JOIN team ON team.name_ = player.team_name
-        INNER JOIN goal ON goal.id = event_.id
-        WHERE team.name_ = away_team.name_ AND event_.game_id = game.id AND event_.id = goal.id) AS Away_Team_Score
-FROM game
-INNER JOIN team home_team ON home_team.name_ = game.home_team_name
-INNER JOIN team away_team ON away_team.name_ = game.away_team_name
-INNER JOIN stadium ON stadium.name_ = game.stadium_name) where (Home_Team_Score > Away_Team_Score AND Home_Team = team.name_) OR (Away_Team_Score > Home_Team_Score AND Away_Team = team.name_))*3 +
-(SELECT COUNT(*) FROM
-(SELECT game.date_ as Date, stadium.name_ as Stadium, home_team.name_ as Home_Team, away_team.name_ as Away_Team,
-       (SELECT COUNT(*) FROM event_
-        INNER JOIN player ON player.id = event_.player_id
-        INNER JOIN team ON team.name_ = player.team_name
-        INNER JOIN goal ON goal.id = event_.id
-        WHERE team.name_ = home_team.name_ AND event_.game_id = game.id AND event_.id = goal.id) AS Home_Team_Score,
-       (SELECT COUNT(*) FROM event_
-        INNER JOIN player ON player.id = event_.player_id
-        INNER JOIN team ON team.name_ = player.team_name
-        INNER JOIN goal ON goal.id = event_.id
-        WHERE team.name_ = away_team.name_ AND event_.game_id = game.id AND event_.id = goal
-        .id) AS Away_Team_Score
-FROM game
-INNER JOIN team home_team ON home_team.name_ = game.home_team_name
-INNER JOIN team away_team ON away_team.name_ = game.away_team_name
-INNER JOIN stadium ON stadium.name_ = game.stadium_name) where (Home_Team_Score = Away_Team_Score AND Home_Team = team.name_) OR ( Away_Team_Score = Home_Team_Score AND Away_Team = team.name_))) AS Points
-FROM team
-Group by team.name_
-ORDER BY Points DESC;
+.headers on
+
+select row_number() over (order by Points desc) as Position, Name, Points from
+(select team.name_ as Name, ((select count(*) from
+    (select game.date_ as Date, stadium.name_ as Stadium, home_team.name_ as Home_Team, away_team.name_ as Away_Team,
+       (select count(*) from event_
+        join player on player.id = event_.player_id
+        join team on team.name_ = player.team_name
+        join goal on goal.id = event_.id
+        where team.name_ = home_team.name_ and event_.game_id = game.id and event_.id = goal.id) as Home_Team_Score,
+       (select count(*) from event_
+        join player on player.id = event_.player_id
+        join team on team.name_ = player.team_name
+        join goal on goal.id = event_.id
+        where team.name_ = away_team.name_ and event_.game_id = game.id and event_.id = goal.id) as Away_Team_Score
+from game
+join team home_team on home_team.name_ = game.home_team_name
+join team away_team on away_team.name_ = game.away_team_name
+join stadium on stadium.name_ = game.stadium_name) where (Home_Team_Score > Away_Team_Score and Home_Team = team.name_) or (Away_Team_Score > Home_Team_Score and Away_Team = team.name_))*3 +
+(select count(*) from
+(select game.date_ as Date, stadium.name_ as Stadium, home_team.name_ as Home_Team, away_team.name_ as Away_Team,
+       (select count(*) from event_
+        join player on player.id = event_.player_id
+        join team on team.name_ = player.team_name
+        join goal on goal.id = event_.id
+        where team.name_ = home_team.name_ and event_.game_id = game.id and event_.id = goal.id) as Home_Team_Score,
+       (select count(*) from event_
+        join player on player.id = event_.player_id
+        join team on team.name_ = player.team_name
+        join goal on goal.id = event_.id
+        where team.name_ = away_team.name_ and event_.game_id = game.id and event_.id = goal
+        .id) as Away_Team_Score
+from game
+join team home_team on home_team.name_ = game.home_team_name
+join team away_team on away_team.name_ = game.away_team_name
+join stadium on stadium.name_ = game.stadium_name) where (Home_Team_Score = Away_Team_Score and Home_Team = team.name_) or ( Away_Team_Score = Home_Team_Score and Away_Team = team.name_))) as Points
+from team
+group by team.name_
+order by Points desc)
+order by Points desc;
--- sql_files/int6.sql
+++ sql_files/int6.sql
@@ -1,42 +1,45 @@
-.mode columns
-.headers on
+.mode column
+.headers off
 .nullvalue NULL
 
--- list the teams that will go to the european competitions
+select "Teams that will go to the european competitions";
+select "";
 
-SELECT team.name_ as name, SUM((SELECT COUNT(*) FROM
-    (SELECT game.date_ as Date, stadium.name_ as Stadium, home_team.name_ as Home_Team, away_team.name_ as Away_Team,
-       (SELECT COUNT(*) FROM event_
-        INNER JOIN player ON player.id = event_.player_id
-        INNER JOIN team ON team.name_ = player.team_name
-        INNER JOIN goal ON goal.id = event_.id
-        WHERE team.name_ = home_team.name_ AND event_.game_id = game.id AND event_.id = goal.id) AS Home_Team_Score,
-       (SELECT COUNT(*) FROM event_
-        INNER JOIN player ON player.id = event_.player_id
-        INNER JOIN team ON team.name_ = player.team_name
-        INNER JOIN goal ON goal.id = event_.id
-        WHERE team.name_ = away_team.name_ AND event_.game_id = game.id AND event_.id = goal.id) AS Away_Team_Score
-FROM game
-INNER JOIN team home_team ON home_team.name_ = game.home_team_name
-INNER JOIN team away_team ON away_team.name_ = game.away_team_name
-INNER JOIN stadium ON stadium.name_ = game.stadium_name) where (Home_Team_Score > Away_Team_Score AND Home_Team = team.name_) OR (Away_Team_Score > Home_Team_Score AND Away_Team = team.name_) )*3+
-(SELECT COUNT(*) FROM
-(SELECT game.date_ as Date, stadium.name_ as Stadium, home_team.name_ as Home_Team, away_team.name_ as Away_Team,
-       (SELECT COUNT(*) FROM event_
-        INNER JOIN player ON player.id = event_.player_id
-        INNER JOIN team ON team.name_ = player.team_name
-        INNER JOIN goal ON goal.id = event_.id
-        WHERE team.name_ = home_team.name_ AND event_.game_id = game.id AND event_.id = goal.id) AS Home_Team_Score,
-       (SELECT COUNT(*) FROM event_
-        INNER JOIN player ON player.id = event_.player_id
-        INNER JOIN team ON team.name_ = player.team_name
-        INNER JOIN goal ON goal.id = event_.id
-        WHERE team.name_ = away_team.name_ AND event_.game_id = game.id AND event_.id = goal.id) AS Away_Team_Score
-FROM game
-INNER JOIN team home_team ON home_team.name_ = game.home_team_name
-INNER JOIN team away_team ON away_team.name_ = game.away_team_name
-INNER JOIN stadium ON stadium.name_ = game.stadium_name) where (Home_Team_Score = Away_Team_Score AND Home_Team = team.name_) OR ( Away_Team_Score = Home_Team_Score AND Away_Team = team.name_))) AS Points
-FROM team
-Group by team.name_
-ORDER BY Points DESC
-LIMIT 3;
+.headers on
+
+select team.name_ as Team, SUM((select COUNT(*) from
+    (select game.date_ as Date, stadium.name_ as Stadium, home_team.name_ as Home_Team, away_team.name_ as Away_Team,
+       (select COUNT(*) from event_
+        join player on player.id = event_.player_id
+        join team on team.name_ = player.team_name
+        join goal on goal.id = event_.id
+        where team.name_ = home_team.name_ and event_.game_id = game.id and event_.id = goal.id) as Home_Team_Score,
+       (select COUNT(*) from event_
+        join player on player.id = event_.player_id
+        join team on team.name_ = player.team_name
+        join goal on goal.id = event_.id
+        where team.name_ = away_team.name_ and event_.game_id = game.id and event_.id = goal.id) as Away_Team_Score
+from game
+join team home_team on home_team.name_ = game.home_team_name
+join team away_team on away_team.name_ = game.away_team_name
+join stadium on stadium.name_ = game.stadium_name) where (Home_Team_Score > Away_Team_Score and Home_Team = team.name_) or (Away_Team_Score > Home_Team_Score and Away_Team = team.name_) )*3+
+(select COUNT(*) from
+(select game.date_ as Date, stadium.name_ as Stadium, home_team.name_ as Home_Team, away_team.name_ as Away_Team,
+       (select COUNT(*) from event_
+        join player on player.id = event_.player_id
+        join team on team.name_ = player.team_name
+        join goal on goal.id = event_.id
+        where team.name_ = home_team.name_ and event_.game_id = game.id and event_.id = goal.id) as Home_Team_Score,
+       (select COUNT(*) from event_
+        join player on player.id = event_.player_id
+        join team on team.name_ = player.team_name
+        join goal on goal.id = event_.id
+        where team.name_ = away_team.name_ and event_.game_id = game.id and event_.id = goal.id) as Away_Team_Score
+from game
+join team home_team on home_team.name_ = game.home_team_name
+join team away_team on away_team.name_ = game.away_team_name
+join stadium on stadium.name_ = game.stadium_name) where (Home_Team_Score = Away_Team_Score and Home_Team = team.name_) or ( Away_Team_Score = Home_Team_Score and Away_Team = team.name_))) as Points
+from team
+group by team.name_
+order BY Points desc
+limit 3;
