04263a411654eb198e358e79ef986cf626795a1f
Gonçalo Miranda <goncalo.miranda.cplx@gmail.com>
2022-12-21 00:36:49 +0000

    Added query 7

--- sql_files/int7.sql
+++ sql_files/int7.sql
@@ -2,27 +2,26 @@
 .headers on
 .nullvalue NULL
 
---Find the names and ages of all players who have scored a goal in a game that ended in a draw.
+select "All the players who scored in draws";
+select "";
 
-SELECT p.name_, p.age
-FROM person p
-JOIN player pl ON p.id = pl.id
-JOIN goal g ON g.player_id = pl.id
-JOIN game ga ON ga.id = g.game_id
-WHERE (SELECT COUNT(*) FROM
-(SELECT game.date_ as Date, stadium.name_ as Stadium, home_team.name_ as Home_Team, away_team.name_ as Away_Team,
-       (SELECT COUNT(*) FROM event_
-        INNER JOIN player ON player.id = event_.player_id
-        INNER JOIN team ON team.name_ = player.team_name
-        INNER JOIN goal ON goal.id = event_.id
-        WHERE team.name_ = home_team.name_ AND event_.game_id = game.id AND event_.id = goal.id) AS Home_Team_Score,
-       (SELECT COUNT(*) FROM event_
-        INNER JOIN player ON player.id = event_.player_id
-        INNER JOIN team ON team.name_ = player.team_name
-        INNER JOIN goal ON goal.id = event_.id
-        WHERE team.name_ = away_team.name_ AND event_.game_id = game.id AND event_.id = goal
-        .id) AS Away_Team_Score
-FROM game
-INNER JOIN team home_team ON home_team.name_ = game.home_team_name
-INNER JOIN team away_team ON away_team.name_ = game.away_team_name
-INNER JOIN stadium ON stadium.name_ = game.stadium_name) where (Home_Team_Score = Away_Team_Score AND Home_Team = team.name_) OR ( Away_Team_Score = Home_Team_Score AND Away_Team = team.name_))
+.headers on
+
+
+select player.name, count(*) as Goals from 
+(select game.date_ as Date, stadium.name_ as Stadium, home_team.name_ as Home_Team, away_team.name_ as Away_Team,
+       (select count(*) from event_
+        join player on player.id = event_.player_id
+        join team on team.name_ = player.team_name
+        join goal on goal.id = event_.id
+        where team.name_ = home_team.name_ and event_.game_id = game.id and event_.id = goal.id) as Home_Team_Score,
+       (select count(*) from event_
+        join player on player.id = event_.player_id
+        join team on team.name_ = player.team_name
+        join goal on goal.id = event_.id
+        where team.name_ = away_team.name_ and event_.game_id = game.id and event_.id = goal
+        .id) as Away_Team_Score
+from game
+join team home_team on home_team.name_ = game.home_team_name
+join team away_team on away_team.name_ = game.away_team_name
+join stadium on stadium.name_ = game.stadium_name) where (Home_Team_Score = Away_Team_Score and Home_Team = team.name_) or ( Away_Team_Score = Home_Team_Score and Away_Team = team.name_) and player_id = event_.player_id
