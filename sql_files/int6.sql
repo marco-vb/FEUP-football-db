7e59634d422d661a0b47a3b0d6f0ac46724267f5
Duarte Gonçalves <up202108772@edu.fe.up.pt>
2022-12-21 15:24:13 +0000

    Added Home and Away score to query 6

--- sql_files/int6.sql
+++ sql_files/int6.sql
@@ -7,6 +7,10 @@ select "";
 
 .headers on
 
-select g.round_id as Round, g.date_ as Date, g.stadium_name as Stadium, g.home_team_name as "Home Team", g.away_team_name as "Away Team"
+select g.round_id as Round, g.date_ as Date, g.stadium_name as Stadium, g.home_team_name as "Home Team", g.away_team_name as "Away Team",
+(select count(*) from goal, event_ e, player p 
+where goal.id = e.id and e.player_id = p.id and p.team_name = g.home_team_name and e.game_id = g.id) as "Home Score",
+(select count(*) from goal, event_ e, player p 
+where goal.id = e.id and e.player_id = p.id and p.team_name = g.away_team_name and e.game_id = g.id) as "Away Score"
 from game g, referee r, person p
 where r.id = p.id and g.referee_id = r.id and p.name_ = "Nuno Almeida"
