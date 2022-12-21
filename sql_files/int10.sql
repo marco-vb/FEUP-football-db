c5a808265f5e133ec9f699de7f533a908427616e
Duarte Gonçalves <up202108772@edu.fe.up.pt>
2022-12-21 14:37:16 +0000

    Query 10 done

--- sql_files/int10.sql
+++ sql_files/int10.sql
@@ -2,7 +2,42 @@
 .headers off
 .nullvalue NULL
 
-select "Percentagem de jogos ganhos fora por cada equipa";
+select "Percentage of games won when playing away";
 select "";
 
 .headers on
+
+select team.name_ as Name, round((select count(*) from
+    (select count(*) from
+    (select game.date_ as Date, stadium.name_ as Stadium, home_team.name_ as Home_Team, away_team.name_ as Away_Team,
+    (select count(*) from event_
+        join player on player.id = event_.player_id
+        join team on team.name_ = player.team_name
+        join goal on goal.id = event_.id
+        where team.name_ = home_team.name_ and event_.game_id = game.id and event_.id = goal.id) as Home_Team_Score,
+    (select count(*) from event_
+        join player on player.id = event_.player_id
+        join team on team.name_ = player.team_name
+        join goal on goal.id = event_.id
+        where team.name_ = away_team.name_ and event_.game_id = game.id and event_.id = goal.id) as Away_Team_Score
+    from game
+    join team home_team on home_team.name_ = game.home_team_name
+    join team away_team on away_team.name_ = game.away_team_name
+    join stadium on stadium.name_ = game.stadium_name) where Away_Team_Score < Home_Team_Score and Away_Team = team.name_)) * 100 /
+    (select count(*) from
+    (select game.date_ as Date, stadium.name_ as Stadium, home_team.name_ as Home_Team, away_team.name_ as Away_Team,
+    (select count(*) from event_
+        join player on player.id = event_.player_id
+        join team on team.name_ = player.team_name
+        join goal on goal.id = event_.id
+        where team.name_ = home_team.name_ and event_.game_id = game.id and event_.id = goal.id) as Home_Team_Score,
+    (select count(*) from event_
+        join player on player.id = event_.player_id
+        join team on team.name_ = player.team_name
+        join goal on goal.id = event_.id
+        where team.name_ = away_team.name_ and event_.game_id = game.id and event_.id = goal.id) as Away_Team_Score
+    from game
+    join team home_team on home_team.name_ = game.home_team_name
+    join team away_team on away_team.name_ = game.away_team_name
+    join stadium on stadium.name_ = game.stadium_name) where (Home_Team_Score > Away_Team_Score and Home_Team = team.name_) or (Away_Team_Score > Home_Team_Score and Away_Team = team.name_)), 2) as Percentage
+from team;
